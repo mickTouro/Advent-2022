01547c3ca0ac8e49694e42b98ac5f62509d3b388
mickTouro <mickovitz@yahoo.com>
2022-12-06 10:53:01 -0500

    Update Advent_06.java

--- 06/Advent_06.java
+++ 06/Advent_06.java
@@ -4,32 +4,20 @@ import java.util.LinkedList;
 import java.util.HashSet;
 public class Advent_06{
     public static void main(String[] args){
-        System.out.println(getCharNum(getInputString()));
-        System.out.println(getCharNum14(getInputString()));
+        System.out.println(getCharNum(4, getInputString()));
+        System.out.println(getCharNum(14, getInputString()));
     }
 
-    public static int getCharNum14(String s){
+    public static int getCharNum(int num, String s){
         Queue<Character> fifo = new LinkedList<Character>();
         char[] chars = s.toCharArray();
-        for(int i = 0; i < 14; i++) { fifo.add(chars[i]); }
-        for(int i = 14; i < chars.length; i++){
+        for(int i = 0; i < num; i++) { fifo.add(chars[i]); }
+        for(int i = num; i < chars.length; i++){
             if(!checkQueueForDups(fifo)) return i; 
             else{fifo.remove(); fifo.add(chars[i]);}
         }
         return 0;
     }
-
-    public static int getCharNum(String s){
-        Queue<Character> fifo = new LinkedList<Character>();
-        char[] chars = s.toCharArray();
-        fifo.add(chars[0]);fifo.add(chars[1]);fifo.add(chars[2]);fifo.add(chars[3]);
-        for(int i = 4; i < chars.length; i++){
-            if(!checkQueueForDups(fifo)) return i; 
-            else{fifo.remove(); fifo.add(chars[i]);}
-        }
-        return 0;
-    }
-
     public static boolean checkQueueForDups(Queue<Character> q){
         Set<Character> s = new HashSet<Character>();
         for(char c : q){
